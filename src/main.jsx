3f14e5942c4824d8f346ba108cf458369cbb883b
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2024-03-18 20:55:06 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/PowerPulseTeamPoject">
+        <BrowserRouter basename="/fs-90-react-node-DeadMakar">
           <App />
         </BrowserRouter>
       </PersistGate>
